1553eb56f432b77c5e3155c95257decd351ba5f5
Cure-X <admin@cure-x.net>
2024-07-29 22:24:22 +0800

    add android build system

---
+++ cmake/android-arm-64.cmake
@@ -0,0 +1,3 @@
+set(COMPILER_DIR ${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/linux-x86_64/bin)
+set(CMAKE_C_COMPILER ${COMPILER_DIR}/aarch64-linux-android34-clang)
+set(CMAKE_CXX_COMPILER ${COMPILER_DIR}/aarch64-linux-android34-clang++)
